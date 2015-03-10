858d0113bd745dc75e2ec37d9f7b86201bbf7ce4
Barak Michaely <barakm18@gmail.com>
2015-03-10 12:04:39 -0400

    Added PlayerControl.js

---
+++ Assets/PlayerControl.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+	if (Input.GetMouseButtonDown(0)) {
+		Jump();
+	}
+	
+	
+}
+
+function Jump() {
+	//Make it Jump
+	this.rigidbody.AddForce(Vector3(0,400,0));
+}
